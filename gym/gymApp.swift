304fa1c11b57b64fbbd5627233c58677817c2547
YUFOHON <yukehan8@gmail.com>
2024-05-21 12:45:17 +0800

    Initial Commit

---
+++ gym/gymApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymApp.swift
+//  gym
+//
+//  Created by Fo Hon Yu on 21/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct gymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
